342bb843a7b58ff07b5f267087fc0823880ad41f
Vishank Rajput <41511518+VishankR@users.noreply.github.com>
2022-01-22 23:29:04 +0530

    Refactored mdstables.sql on Jan 22, 2022

--- src/main/resources/mdsTables.sql
+++ src/main/resources/mdsTables.sql
@@ -1,83 +1,72 @@
-CREATE TABLE `mds`.`patient` (
-  `patient_id` INT NOT NULL AUTO_INCREMENT,
-  `dob` DATETIME NOT NULL,
-  `home_contact` INT NULL,
-  `mobile_contact` INT NULL,
-  `office_contact` INT NULL,
-  `first_name` VARCHAR(45) NOT NULL,
-  `middle_name` VARCHAR(45) NULL,
-  `last_name` VARCHAR(45) NULL,
-  `gender` VARCHAR(45) NULL,
-  `mail` VARCHAR(45) NULL,
-  `created_date` DATETIME NULL,
-  `created_by` VARCHAR(45) NULL,
-  `last_updated_date` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
-  `last_updated_by` VARCHAR(45) NULL,
-  `is_dirty` VARCHAR(1) NULL DEFAULT 'N',
+-- mds.patient definition
+CREATE TABLE `patient` (
+  `patient_id` int NOT NULL AUTO_INCREMENT,
+  `dob` datetime NOT NULL,
+  `home_contact` double DEFAULT NULL,
+  `mobile_contact` double DEFAULT NULL,
+  `office_contact` double DEFAULT NULL,
+  `first_name` varchar(45) NOT NULL,
+  `middle_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `gender` varchar(45) DEFAULT NULL,
+  `mail` varchar(45) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `created_by` varchar(45) DEFAULT NULL,
+  `last_updated_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `last_updated_by` varchar(45) DEFAULT NULL,
+  `is_dirty` varchar(1) DEFAULT 'N',
   PRIMARY KEY (`patient_id`),
-  UNIQUE INDEX `patient_id_UNIQUE` (`patient_id` ASC) VISIBLE);
+  UNIQUE KEY `patient_id_UNIQUE` (`patient_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-  CREATE TABLE `mds`.`patient_address` (
-  `street` VARCHAR(45) NULL,
-  `city` VARCHAR(45) NULL,
-  `state` VARCHAR(45) NULL,
-  `patient_id` INT NOT NULL,
-  INDEX `patient_patientAddress_idx` (`patient_id` ASC) VISIBLE,
-  CONSTRAINT `patient_patientAddress`
-    FOREIGN KEY (`patient_id`)
-    REFERENCES `mds`.`patient` (`patient_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+-- mds.patient_address definition
+CREATE TABLE `patient_address` (
+  `street` varchar(45) DEFAULT NULL,
+  `city` varchar(45) DEFAULT NULL,
+  `state` varchar(45) DEFAULT NULL,
+  `patient_id` int NOT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `created_by` varchar(45) DEFAULT NULL,
+  `last_updated_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `last_updated_by` varchar(45) DEFAULT NULL,
+  KEY `patient_patientAddress_idx` (`patient_id`),
+  CONSTRAINT `patient_patientAddress` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`patient_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-    CREATE TABLE `mds`.`prescription` (
-  `prescription_id` INT NOT NULL AUTO_INCREMENT,
-  `patient_id` INT NOT NULL,
-  `drug_name` VARCHAR(45) NOT NULL,
-  `dispensed_quantity` INT NULL,
-  `days` INT NULL,
-  `refills` INT NULL,
-  `prescribed_quantity` INT NULL,
-  `refill_quantity` INT NULL,
-  `created_date` DATETIME NULL,
-  `created_by` VARCHAR(45) NULL,
-  `last_updated_date` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
-  `last_updated_by` VARCHAR(45) NULL,
+-- mds.prescription definition
+CREATE TABLE `prescription` (
+  `prescription_id` int NOT NULL AUTO_INCREMENT,
+  `patient_id` int NOT NULL,
+  `drug_name` varchar(45) NOT NULL,
+  `dispensed_quantity` int DEFAULT NULL,
+  `days` int DEFAULT NULL,
+  `refills` int DEFAULT NULL,
+  `prescribed_quantity` int DEFAULT NULL,
+  `refill_quantity` int DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `created_by` varchar(45) DEFAULT NULL,
+  `last_updated_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `last_updated_by` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`prescription_id`),
-  UNIQUE INDEX `prescription_id_UNIQUE` (`prescription_id` ASC) VISIBLE,
-  INDEX `prescription_patient_idx` (`patient_id` ASC) VISIBLE,
-  CONSTRAINT `prescription_patient`
-    FOREIGN KEY (`patient_id`)
-    REFERENCES `mds`.`patient` (`patient_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+  UNIQUE KEY `prescription_id_UNIQUE` (`prescription_id`),
+  KEY `prescription_patient_idx` (`patient_id`),
+  CONSTRAINT `prescription_patient` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`patient_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `mds`.`prescription_fill` (
-  `prescription_fill_id` INT NOT NULL AUTO_INCREMENT,
-  `prescription_id` INT NOT NULL,
-  `days` INT NULL,
-  `dispensed_quantity` INT NULL,
-  `fill_number` INT NULL,
-  `pickup_date` DATETIME NULL,
-  `created_date` DATETIME NULL,
-  `created_by` VARCHAR(45) NULL,
-  `last_updated_date` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
-  `last_updated_by` VARCHAR(45) NULL,
+-- mds.prescription_fill definition
+CREATE TABLE `prescription_fill` (
+  `prescription_fill_id` int NOT NULL AUTO_INCREMENT,
+  `prescription_id` int NOT NULL,
+  `days` int DEFAULT NULL,
+  `dispensed_quantity` int DEFAULT NULL,
+  `fill_number` int DEFAULT NULL,
+  `pickup_date` datetime DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `created_by` varchar(45) DEFAULT NULL,
+  `last_updated_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `last_updated_by` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`prescription_fill_id`),
-  UNIQUE INDEX `prescription_fill_id_UNIQUE` (`prescription_fill_id` ASC) VISIBLE,
-  INDEX `prescriptionFill_prescription_idx` (`prescription_id` ASC) VISIBLE,
-  CONSTRAINT `prescriptionFill_prescription`
-    FOREIGN KEY (`prescription_id`)
-    REFERENCES `mds`.`prescription` (`prescription_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
-ALTER TABLE `mds`.`patient_address` 
-ADD COLUMN `created_date` DATETIME NULL AFTER `patient_id`,
-ADD COLUMN `created_by` VARCHAR(45) NULL AFTER `created_date`,
-ADD COLUMN `last_updated_date` DATETIME NULL DEFAULT CURRENT_TIMESTAMP AFTER `created_by`,
-ADD COLUMN `last_updated_by` VARCHAR(45) NULL AFTER `last_updated_date`;
-
-ALTER TABLE `mds`.`patient` 
-CHANGE COLUMN `home_contact` `home_contact` DOUBLE NULL DEFAULT NULL ,
-CHANGE COLUMN `mobile_contact` `mobile_contact` DOUBLE NULL DEFAULT NULL ,
-CHANGE COLUMN `office_contact` `office_contact` DOUBLE NULL DEFAULT NULL ;
+  UNIQUE KEY `prescription_fill_id_UNIQUE` (`prescription_fill_id`),
+  KEY `prescriptionFill_prescription_idx` (`prescription_id`),
+  CONSTRAINT `prescriptionFill_prescription` FOREIGN KEY (`prescription_id`) REFERENCES `prescription` (`prescription_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
