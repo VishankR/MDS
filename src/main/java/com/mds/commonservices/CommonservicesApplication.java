c3b9e55effb27344804b4a4a2dcb840243896834
Vishank Rajput <41511518+VishankR@users.noreply.github.com>
2022-02-11 00:37:36 +0530

    Refactored again on Feb 11 , 2022

--- src/main/java/com/mds/commonservices/CommonservicesApplication.java
+++ src/main/java/com/mds/commonservices/CommonservicesApplication.java
@@ -6,10 +6,13 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @EnableJpaRepositories("com.mds.commonservices.repositories")
 @ConfigurationPropertiesScan({ "com.mds.commonservices.resource"})
 @SpringBootApplication
 @EnableFeignClients
+@EnableSwagger2
 public class CommonservicesApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/mds/commonservices/mockData/Data.java
+++ src/main/java/com/mds/commonservices/mockData/Data.java
@@ -12,99 +12,73 @@ import com.mds.entities.Prescription;
 import com.mds.entities.PrescriptionFill;
 
 public class Data {
-    public static PatientAddress getDummyPatientAddress(){
-        PatientAddress patAdd = new PatientAddress();
-        patAdd.setCity("Bijnor");
-        patAdd.setStreet("H.No. - 14, Karimpur Nangli");
-        patAdd.setState("UP");
-        return patAdd;
-    }
-    public static List<PatientAddress> getDummyPatientAddressList(){
-        List<PatientAddress> temp = new ArrayList<>();
-        PatientAddress patAdd = new PatientAddress();
-        patAdd.setCity("Bijnor");
-        patAdd.setStreet("H.No. - 14, Karimpur Nangli");
-        patAdd.setState("UP");
-        temp.add(patAdd);
-        return temp;
-    }
-    public static Prescription getDummyPrescription(Patient pat, List<PrescriptionFill> presFill){
-        Prescription patPres = new Prescription();
-        patPres.setDays(30);
-        patPres.setDispensedQuantity(30);
-        patPres.setDrugName("Paracetamol");
-        patPres.setPatient(pat);
-        patPres.setPrescribedQuantity(90);
-        //patPres.setPresFill(presFill);
-        patPres.setRefillQuantity(30);
-        patPres.setRefills(2);
-        return patPres;
-    }
-    public static List<Prescription> getDummyPrescriptionList(Patient pat, List<PrescriptionFill> presFill){
-        List<Prescription> temp = new ArrayList<>();
-        Prescription patPres = new Prescription();
-        patPres.setDays(30);
-        patPres.setDispensedQuantity(30);
-        patPres.setDrugName("Paracetamol");
-        patPres.setPatient(pat);
-        patPres.setPrescribedQuantity(90);
-        patPres.setPresFill(presFill);
-        presFill.forEach(tempPresFill -> tempPresFill.setPres(patPres));
-        patPres.setRefillQuantity(30);
-        patPres.setRefills(2);
-        temp.add(patPres);
-        return temp;
-    }
-    public static PrescriptionFill getDummyPrescriptionFill(){
-        PrescriptionFill patPresFill = new PrescriptionFill();
-        patPresFill.setDays(30);
-        patPresFill.setDispensedQuantity(30);
-        patPresFill.setFillNumber(0);
-        patPresFill.setPickupDate(new Date());
-        //patPresFill.setPres(pres);
-       // patPresFill.setRefill_number(0);
-        return patPresFill;
-    }
-    public static List<PrescriptionFill> getDummyPrescriptionFillList(){
-        List<PrescriptionFill> temp = new ArrayList<>();
-        PrescriptionFill patPresFill = new PrescriptionFill();
-        patPresFill.setDays(30);
-        patPresFill.setDispensedQuantity(30);
-        patPresFill.setFillNumber(0);
-        patPresFill.setPickupDate(new Date());
-        //patPresFill.setRefill_number(0);
-        temp.add(patPresFill);
-        return temp;
-    }
-    public static List<PrescriptionFill> getDummyPrescriptionFillListWithPrescription(Prescription pres){
-        List<PrescriptionFill> temp = new ArrayList<>();
-        PrescriptionFill patPresFill = new PrescriptionFill();
-        patPresFill.setDays(30);
-        patPresFill.setDispensedQuantity(30);
-        patPresFill.setFillNumber(2);
-        patPresFill.setPickupDate(new Date());
-       // patPresFill.setRefill_number(1);
-        patPresFill.setPres(pres);
-        temp.add(patPresFill);
-        return temp;
-    }
-    public static Contact getDummyPatientContact(){
-        Contact patContact = new Contact();
-        patContact.setMobileContact("8510964757");
-        return patContact;
-    }
-    public static Patient getDummyPatient(){
-        Patient pat= new Patient();
-        pat.setAddress(getDummyPatientAddressList());
-        //pat.setAge(25);
-        pat.setBirthDate(new Date(1998, Calendar.JANUARY, 5));
-        pat.setContact(getDummyPatientContact());
-        pat.setFirstName("Vishank");
-        pat.setLastName("Rajput");
-        pat.setGender("Male");
-        pat.setContact(getDummyPatientContact());
-        pat.setMail("vishankkumar@gmail.com");
-        pat.setValid(true);
-        return pat;
-    }
+
+	public static List<PatientAddress> getDummyPatientAddressList() {
+		List<PatientAddress> temp = new ArrayList<>();
+		PatientAddress patAdd = new PatientAddress();
+		patAdd.setCity("Bijnor");
+		patAdd.setStreet("H.No. - 14, Karimpur Nangli");
+		patAdd.setState("UP");
+		temp.add(patAdd);
+		return temp;
+	}
+
+	public static List<Prescription> getDummyPrescriptionList(List<PrescriptionFill> presFillList) {
+		List<Prescription> temp = new ArrayList<>();
+		Prescription patPres = new Prescription();
+		patPres.setDays(30);
+		patPres.setDispensedQuantity(30);
+		patPres.setDrugName("Paracetamol");
+		patPres.setPrescribedQuantity(90);
+		patPres.setPresFillList(presFillList);
+		patPres.setRefillQuantity(30);
+		patPres.setRefills(2);
+		temp.add(patPres);
+		return temp;
+	}
+
+	public static List<PrescriptionFill> getDummyPrescriptionFillList() {
+		List<PrescriptionFill> temp = new ArrayList<>();
+		PrescriptionFill patPresFill = new PrescriptionFill();
+		patPresFill.setDays(30);
+		patPresFill.setDispensedQuantity(30);
+		patPresFill.setFillNumber(0);
+		patPresFill.setPickupDate(new Date());
+		patPresFill.setFillNumber(0);
+		temp.add(patPresFill);
+		return temp;
+	}
+
+	public static List<PrescriptionFill> getDummyPrescriptionFillListWithPrescription(Prescription pres) {
+		List<PrescriptionFill> temp = new ArrayList<>();
+		PrescriptionFill patPresFill = new PrescriptionFill();
+		patPresFill.setDays(30);
+		patPresFill.setDispensedQuantity(30);
+		patPresFill.setFillNumber(2);
+		patPresFill.setPickupDate(new Date());
+		patPresFill.setFillNumber(1);
+		patPresFill.setPres(pres);
+		temp.add(patPresFill);
+		return temp;
+	}
+
+	public static Contact getDummyPatientContact() {
+		Contact patContact = new Contact();
+		patContact.setMobileContact("8510964757");
+		return patContact;
+	}
+
+	public static Patient getDummyPatient() {
+		Patient pat = new Patient();
+		pat.setAddress(getDummyPatientAddressList());
+		pat.setBirthDate(new Date(1998, Calendar.JANUARY, 5));
+		pat.setContact(getDummyPatientContact());
+		pat.setFirstName("Vishank");
+		pat.setLastName("Rajput");
+		pat.setGender("Male");
+		pat.setMail("vishankkumar@gmail.com");
+		pat.setValid(true);
+		pat.setPres(getDummyPrescriptionList(getDummyPrescriptionFillList()));
+		return pat;
+	}
 }
